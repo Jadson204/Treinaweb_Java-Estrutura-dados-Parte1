5b4ca77a455c961ed4a18cb1b476f12e073b3828
Jadson204 <jadsonhenrique2004@gmail.com>
2024-04-14 11:57:55 -0300

    Estruturas de Dados - listas duplamente ligadas: modificando os métodos de remoção

--- src/br/com/treinaweb/estruturadados/listasligadas/ListaDuplamenteLigada.java
+++ src/br/com/treinaweb/estruturadados/listasligadas/ListaDuplamenteLigada.java
@@ -102,17 +102,21 @@ public class ListaDuplamenteLigada<T> {
         if (posicao == 0) {
             No<T> proximoNo = this.primeiroNo.getProximo();
             this.primeiroNo.setProximo(null);
+            proximoNo.setAnterior(null);
             this.primeiroNo = proximoNo;
         } else if (posicao == tamanho() -1) {
             No<T> penultimoNo = recuperarNo(tamanho() - 2);
             penultimoNo.setProximo(null);
+            this.ultimoNo.setAnterior(null);
             this.ultimoNo = penultimoNo;
         } else {
-            No<T> noAnterior = recuperarNo(posicao - 1);
-            No<T> proximoNo = recuperarNo(posicao + 1);
             No<T> noAtual = recuperarNo(posicao);
+            No<T> noAnterior = recuperarNo(posicao - 1);
+            No<T> proximoNo = noAtual.getProximo();
             noAnterior.setProximo(proximoNo);
+            proximoNo.setAnterior(noAnterior);
             noAtual.setProximo(null);
+            noAtual.setAnterior(null);
         }
         this.tamanho--;
     }
